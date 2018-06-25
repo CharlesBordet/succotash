213b4bf999fbd9bc72b4a1d408ef2694c22c7458
VND Bordet <Bordet.Charles@bcg.com>
2018-06-25 17:46:04 +0200

    Started to add UI for adding a new recipe

--- ui.R
+++ ui.R
@@ -4,7 +4,44 @@ dashboardPage(
     title = "Succotash",
     skin = "purple",
 
-    dashboardHeader(),
-    dashboardSidebar(),
-    dashboardBody()
+    dashboardHeader(
+        title = "Succotash"
+    ),
+
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Recipes", tabName = "menu_recipes",
+                     icon = icon("book")),
+            menuItem("Add new recipe", tabName = "menu_add_recipe",
+                     icon = icon("plus"))
+        )
+    ),
+
+    dashboardBody(
+
+        tabItems(
+            tabItem(tabName = "menu_add_recipe",
+
+                    box(title = "New Recipe",
+                        textInput("new_title", "TITLE",
+                                  placeholder = "Title"),
+                        fluidRow(
+                            column(width = 6,
+                                   textInput("new_prep_time", "PREPARATION TIME",
+                                             placeholder = "25 minutes")
+                            ),
+                            column(width = 6,
+                                   textInput("new_yield", "SERVINGS",
+                                             placeholder = "4 cups")
+                            )
+                        )
+                        # checkboxGroupInput("new_tags"),
+                        # list d'ingrédients
+                        # instructions
+                        # photos
+                    )
+            )
+        )
+
+    )
 )
