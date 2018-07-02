2eeda80a5fb43c6545a72d6b2b1c3116d2026fb8
Charles <charles.bordet@gmail.com>
2018-07-02 09:05:52 +0000

    added admin tab

--- global.R
+++ global.R
@@ -1,6 +1,12 @@
+
+library(sqldf)
 library(shiny)
 library(shinyjs)
 library(shinydashboard)
 library(rhandsontable)
 
+
 options(stringsAsFactors = FALSE)
+
+db <- dbConnect(SQLite(), dbname = "succotash.sqlite")
+# https://www.r-bloggers.com/r-and-sqlite-part-1/
--- ui.R
+++ ui.R
@@ -13,7 +13,12 @@ dashboardPage(
             menuItem("Recipes", tabName = "menu_recipes",
                      icon = icon("book")),
             menuItem("Add new recipe", tabName = "menu_add_recipe",
-                     icon = icon("plus"))
+                     icon = icon("plus")),
+            menuItem("Administration", tabName = "admin",
+                     icon = icon("admin"),
+                     menuSubItem("Tags", tabName = "admin_tags"),
+                     menuSubItem("Ingredients", tabName = "admin_ingredients")
+            )
         )
     ),
 
